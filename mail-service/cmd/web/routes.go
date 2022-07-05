87bd44c80d99a31a58972db335d88411c585d736
cuong_nt <cuong-nt@blockhive.vn>
2022-07-05 13:54:40 +0700

    Add mail service structure

---
+++ mail-service/cmd/web/routes.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	chi2 "github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+	"net/http"
+)
+
+func (app *Config) routes() http.Handler {
+	chi := chi2.NewRouter()
+
+	chi.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTION"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content_Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
+
+	chi.Use(middleware.Heartbeat("/ping"))
+
+	return chi
+}
