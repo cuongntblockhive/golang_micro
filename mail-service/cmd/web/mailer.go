238d15a62909dcb69251fd2a5ed228d044c6c129
cuongntblockhive <85241021+cuongntblockhive@users.noreply.github.com>
2022-07-12 21:46:41 +0700

    Update mailer.go

--- mail-service/cmd/web/mailer.go
+++ mail-service/cmd/web/mailer.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"bytes"
+	"github.com/vanng822/go-premailer/premailer"
+	mail "github.com/xhit/go-simple-mail/v2"
+	"time"
+
 	template2 "html/template"
 )
 
@@ -43,6 +47,40 @@ func (m *Mail) SendSMTPMessage(msg Message) error {
 	if err != nil {
 		return err
 	}
+	plainMessage, err := m.buildPlainTextMessage(msg)
+	if err != nil {
+		return err
+	}
+
+	server := mail.NewSMTPClient()
+	server.Host = m.Host
+	server.Port = m.Port
+	server.Username = m.UserName
+	server.Password = m.Password
+	server.Encryption = m.getEncryption(m.Encryption)
+	server.KeepAlive = false
+	server.ConnectTimeout = 10 * time.Second
+	server.SendTimeout = 10 * time.Second
+
+	smtpClient, err := server.Connect()
+	if err != nil {
+		return err
+	}
+
+	email := mail.NewMSG()
+	email.SetFrom(msg.From).AddTo(msg.To).SetSubject(msg.Subject)
+	email.SetBody(mail.TextPlain, plainMessage)
+	email.AddAlternative(mail.TextHTML, fmtMessage)
+	if len(msg.Attachments) > 0 {
+		for _, attachment := range msg.Attachments {
+			email.AddAttachment(attachment)
+		}
+	}
+	err = email.Send(smtpClient)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func (m *Mail) buildHTMLMessage(msg Message) (string, error) {
@@ -65,6 +103,48 @@ func (m *Mail) buildHTMLMessage(msg Message) (string, error) {
 	return fmtMessage, nil
 }
 
-func (m *Mail) inlineCSS(s string) (string, error) {
+func (m *Mail) buildPlainTextMessage(msg Message) (string, error) {
+	template := "template/mail.plain.tmpl"
+	t, err := template2.New("email-plain").ParseFiles(template)
+	if err != nil {
+		return "", err
+	}
 
+	var tpl bytes.Buffer
+	if err = t.ExecuteTemplate(&tpl, "body", msg.DataMap); err != nil {
+		return "", err
+	}
+
+	plainMessage := tpl.String()
+	return plainMessage, nil
+}
+
+func (m *Mail) inlineCSS(s string) (string, error) {
+	options := premailer.Options{
+		RemoveClasses:     false,
+		CssToAttributes:   false,
+		KeepBangImportant: true,
+	}
+	prem, err := premailer.NewPremailerFromString(s, &options)
+	if err != nil {
+		return "", err
+	}
+	html, err := prem.Transform()
+	if err != nil {
+		return "", err
+	}
+	return html, nil
+}
+
+func (m *Mail) getEncryption(encryption string) mail.Encryption {
+	switch encryption {
+	case "tls":
+		return mail.EncryptionTLS
+	case "ssl":
+		return mail.EncryptionSSLTLS
+	case "none", "":
+		return mail.EncryptionNone
+	default:
+		return mail.EncryptionSTARTTLS
+	}
 }
